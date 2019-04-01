d2905f5b192ee8646865344d6789c7daedd7d34c
Tobin77 <ydp0731@126.com>
2019-04-01 16:41:44 +0800

    Cooperate Office

---
+++ src/main/java/com/wy/cooperate/CooperateApplication.java
@@ -0,0 +1,13 @@
+package com.wy.cooperate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperateApplication.class, args);
+    }
+
+}
